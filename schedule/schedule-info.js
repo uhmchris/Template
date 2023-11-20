df963b9b40e61f457e55f4e7e56a55cc84338fd6
uhmchris <uhmchris@users.noreply.github.com>
2023-11-20 19:59:47 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,19 @@
+moreaEventData = [
+  {title: "E09: Doing good as a software engineer", url: "/Template/morea/example-ethics/experience-se-ethics-good.html", start: "2021-07-27T23:00"},
+  {title: "A03: Assess your ability to reason about ethical issues in software development", url: "/Template/morea/example-ethics/assessment-ethics.html", start: "2021-07-30T09:00"},
+  {title: "E08: What are the professional codes of software engineering ethics?", url: "/Template/morea/example-ethics/experience-se-ethics-codes.html", start: "2021-07-26T23:00"},
+  {title: "E10: Doing harm as a software engineer", url: "/Template/morea/example-ethics/experience-se-ethics-harm.html", start: "2021-07-28T23:00"},
+  {title: "E04: Introduction to Javascript with FreeCodeCamp", url: "/Template/morea/example-javascript/experience-interactive-javascript-1.html", start: "2021-07-13T23:00"},
+  {title: "E06: Reflect on Javascript 1", url: "/Template/morea/example-javascript/experience-javascript-1-technical-essay.html", start: "2021-07-15T23:00"},
+  {title: "A01: Assessment of simple Javascript skills", url: "/Template/morea/example-javascript/assessment-javascript-1.html", start: "2021-07-16T09:00"},
+  {title: "E05: isUnique", url: "/Template/morea/example-javascript/experience-ctci.html", start: "2021-07-14T23:00"},
+  {title: "Module Types", url: "/Template/modules/module-types", start: "2021-07-13", end: "2021-07-16"},
+  {title: "E03: Learn the command line", url: "/Template/morea/example-introduction/experience-learn-the-command-line.html", start: "2021-07-09T23:00"},
+  {title: "E02: Introduce yourself", url: "/Template/morea/example-introduction/experience-introduce-yourself.html", start: "2021-07-08T23:00"},
+  {title: "E01: Environment configuration", url: "/Template/morea/example-introduction/experience-environment-configuration.html", start: "2021-07-07T23:00"},
+  {title: "E07: Reflect on smart questions", url: "/Template/morea/example-open-source-software/experience-smart-questions.html", start: "2021-07-22T23:00"},
+  {title: "A02: Assess your ability to write effectively about open source software", url: "/Template/morea/example-open-source-software/assessment-open-source-software.html", start: "2021-07-23T09:00"},
+  {title: "Module Introduction to Python 3", url: "/Template/modules/module-introduction-to-python-3", start: "2021-07-12", end: "2021-07-16"},
+  {title: "Module Strings", url: "/Template/modules/module-strings", start: "2021-07-15", end: "2021-07-16"},
+  {title: "Module Loops", url: "/Template/modules/module-loops", start: "2021-07-14", end: "2021-07-16"}
+]
